485e048b2667d06e4fdc5df082fb752ddda29bbb
Cyberton207 <shubhamkarale768ms@gmail.com>
2023-03-21 23:30:38 +0530

    Variables Notes On Js

---
+++ VariavblesInJs.js
@@ -0,0 +1,21 @@
+//
+/*
+ 1) Variables are the container for storing the data
+ 2) Js is Case Sensitive
+ 3) Js has some reserved keyword which can not be used as Variables
+ 4) Js has two types for declaring variables a) var,val 2) let and 2nd type is most preferable
+ 5) Js is Dynamically Typed Language means at run time we can change he type of variables
+ */
+
+
+
+let a  = "560"
+console.log(typeof a)
+a = Number(a);
+console.log(typeof(a));
+a = String(a);
+console.log(typeof(a));
+a = Boolean(a);
+console.log(typeof(a));
+a = BigInt(a);
+console.log(typeof(a));
