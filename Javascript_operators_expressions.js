bbb466da2e2d9fe44e24bf387f5ec678c4acd60c
Cyberton207 <shubhamkarale768ms@gmail.com>
2023-03-22 10:31:41 +0530

    Operators and Expressions

---
+++ Javascript_operators_expressions.js
@@ -0,0 +1,21 @@
+/*
+ === is used to check the value + datatypes if any one of the differs then  is returns false
+ == Assignment Operator
+ != not equal to
+ !== not not equal to
+
+ */
+let num1 = 66
+let num2 = "66"
+console.log(num1==num2)
+console.log(num1!=num2) // Returns false beacause one is int and other is string
+console.log(num1===num2)
+console.log(num1!==num2)
+
+
+// Logical Operators
+/*
+1) &&  = if both are true then only true
+2) ||  = if any of true then it's true
+3) ~   =  neagtion Operator
+ */
