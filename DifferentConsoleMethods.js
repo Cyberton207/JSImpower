94ff02815cfa386c78c1be8e212966822186f604
cyberton207 <shubhamkarale768ms@gmail.com>
2023-03-31 10:28:48 +0530

    Commit for these file name Difference_console_methods.

---
+++ DifferentConsoleMethods.js
@@ -0,0 +1,24 @@
+/*
+console.log(_) = It is used for printing the Values
+console.assert(_) = It's used for the checking condition it gives output only when the condition is failed
+console.warn(_) = It gives the warning = It also verify the condition is true or not and it also prints the Value
+console.table(_) = Can print the object into tabular format
+console.info(_) === console.log(_)
+ */
+
+console.log("Hello World");
+console.error("The Error");
+console.warn("These is Warning for future error");
+console.warn(1<2);
+console.assert(1>2);
+console.assert(1<2);
+console.log(console)
+console.clear();
+obj = {
+    "nano":1,
+    "prash":2,
+    "Devu":3,
+    "Ishu":4
+}
+console.table(obj)
+console.timeEnd(obj);
